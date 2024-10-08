9b05b30bc1161ab5fe06af026b56e28eb74bb01f
Breno1101 <brenocoutinhorodrigues@gmail.com>
2024-10-08 18:34:23 -0300

    adjusting configuration on dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export function App() {
   return (
-      <h1 className="text-3xl font-bold">
-          Generic Schedule Generator (GSG)
-      </h1>
+    <div>
+      <h1>Frontend</h1>
+    </div>
   )
 }
