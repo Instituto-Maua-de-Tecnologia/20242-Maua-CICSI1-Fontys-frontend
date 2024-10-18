40b14fe323a214df015fe3a5c04389466d8fd9d2
Amanda <22.00986-8@maua.br>
2024-10-18 18:55:53 -0300

    Professor Screen

--- src/professor/index.tsx
+++ src/professor/index.tsx
@@ -1,26 +1,40 @@
+import React, { useState, ChangeEvent } from 'react';
 import TranslationButtons from '../components/translationButtons';
-import BtnProfessor from '../assets/btn-professor.png';
-function ProfessorScreen(){
-    return(
-        <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-70 flex justify-center items-center">
-        <TranslationButtons/>
-        <div className="grid grid-cols-2 gap-8">
-        <button className="w-64 h-64 bg-blue-900 text-white rounded-lg shadow-md hover:shadow-lg transition duration-300">
-            <div className="text-center text-xl py-4">
-            Inform Availability/Subjects
-            <img src={BtnProfessor}></img>
+import BtnCoordinator from '../assets/btn-professor.png';
+import BtnCoordinator2 from '../assets/btn-professor2.png';
+
+
+function ProfessorScreen() {
+  return (
+    <div className="absolute top-0 left-0 w-full h-full bg-[#E8E9EB] flex flex-col justify-center items-center">
+      <TranslationButtons />
+      <text className='text-center text-3xl text-black mb-8'>Hello, Name!</text>
+      <div className="grid grid-cols-2 gap-20 mb-20">
+        <button
+            className="w-80 h-64 relative rounded-[30px] shadow-md transition-all duration-300 transform hover:scale-110 "
+            style={{ backgroundImage: `url(${BtnCoordinator})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
+          >
+          <div className="absolute inset-0 rounded-[30px] flex justify-center items-center bg-black hover:bg-opacity-30 transition-all duration-300 opacity-0 hover:opacity-100">
+            <div className="text-center text-3xl text-white">
+              Inform Availability/Subjects
             </div>
+          </div>
         </button>
-        
-        <button className="w-64 h-64 bg-blue-900 text-white rounded-lg shadow-md hover:shadow-lg transition duration-300">
-          <div className="text-center text-xl py-4">
+
+
+        <button
+          className="w-80 h-64 relative rounded-[30px] shadow-md  transition-all duration-300 transform hover:scale-110"
+          style={{ backgroundImage: `url(${BtnCoordinator2})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
+        >
+        <div className="absolute inset-0 rounded-[30px] flex justify-center items-center bg-black hover:bg-opacity-30 transition-all duration-300 opacity-0 hover:opacity-100">
+          <div className="text-center text-3xl text-white">
             Visualize Schedule
+          </div>
           </div>
         </button>
       </div>
     </div>
-    );
-
+  );
 }
 
-export default ProfessorScreen
+export default ProfessorScreen;
