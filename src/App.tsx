abdb1d5443d4799b6ec90242a374c4915f61514a
ZettDev <23.00171-2>
2024-10-23 09:03:16 -0300

    New pages routed

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import Login from './login/index';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ProfessorSchedule from './professorSchedule';
 import Professor from './professor';
+import ScheduleTable from './schedule';
+import NotFound from './screens/notFound';
 
 export function App() { 
   return (
@@ -10,6 +12,8 @@ export function App() {
         <Route index element={<Login />} />
         <Route path='professor' element={<Professor/>}/>
         <Route path="professors_schedule" element={<ProfessorSchedule />} />
+        <Route path="schedule" element={<ScheduleTable/>}/>
+        <Route path="*" element={<NotFound/>}/>
       </Routes>
     </BrowserRouter>
   )
