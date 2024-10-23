1fd153e09cf3bbbff9528d38803f0b6977f26134
ZettDev <23.00171-2>
2024-10-23 12:40:41 -0300

    Added LanguageProvider

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,20 @@ import ProfessorSchedule from './professorSchedule';
 import Professor from './professor';
 import ScheduleTable from './schedule';
 import NotFound from './screens/notFound';
+import { LanguageProvider } from './components/languageProvider';
 
 export function App() { 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Login />} />
-        <Route path='professor' element={<Professor/>}/>
-        <Route path="professors_schedule" element={<ProfessorSchedule />} />
-        <Route path="schedule" element={<ScheduleTable/>}/>
-        <Route path="*" element={<NotFound/>}/>
-      </Routes>
-    </BrowserRouter>
+    <LanguageProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Login />} />
+          <Route path='professor' element={<Professor/>}/>
+          <Route path="professors_schedule" element={<ProfessorSchedule />} />
+          <Route path="schedule" element={<ScheduleTable/>}/>
+          <Route path="*" element={<NotFound/>}/>
+        </Routes>
+      </BrowserRouter>
+    </LanguageProvider>
   )
 }
