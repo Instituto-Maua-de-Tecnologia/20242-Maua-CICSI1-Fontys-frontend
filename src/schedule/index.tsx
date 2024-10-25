a817dad78c0f46f7a945fad8f661b1fcdd15733b
Amanda <22.00986-8@maua.br>
2024-10-25 16:47:10 -0300

    schedule professor/ adding observations and change color on click

--- src/schedule/index.tsx
+++ src/schedule/index.tsx
@@ -1,4 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, ChangeEvent } from 'react';
+import TranslationButtons from '../components/translationButtons';
+import {
+  ArrowCircleLeft,
+  MagnifyingGlass,
+  MagnifyingGlassPlus,
+  UserCircle,
+  Check,
+} from "phosphor-react";
 
 const ScheduleTable: React.FC = () => {
   const [availability, setAvailability] = useState({
@@ -9,6 +17,12 @@ const ScheduleTable: React.FC = () => {
     fri: [],
     sat: [],
   });
+  const [selectedAvailability, setSelectedAvailability] = useState('available');
+  const [availableCells, setAvailableCells] = useState<{ [key: string]: number[] }>({});
+
+  const handleAvailabilityChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setSelectedAvailability(event.target.value);
+  };
 
   const times = [
     "7h40 - 9h20", "9h30 - 11h10", "11h20 - 13h00", 
@@ -18,40 +32,68 @@ const ScheduleTable: React.FC = () => {
   const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
   const toggleAvailability = (day: string, index: number) => {
-    const isAvailable = availability[day]?.includes(index);
-    setAvailability(prev => ({
-      ...prev,
-      [day]: isAvailable
-        ? prev[day].filter((i: number) => i !== index)
-        : [...(prev[day] || []), index]
-    }));
+    if (selectedAvailability === 'available') {
+      setAvailableCells(prev => ({
+        ...prev,
+        [day]: prev[day]?.includes(index)
+          ? prev[day].filter((i: number) => i !== index)
+          : [...(prev[day] || []), index]
+      }));
+    } else {
+      setAvailability(prev => ({
+        ...prev,
+        [day]: prev[day]?.includes(index)
+          ? prev[day].filter((i: number) => i !== index)
+          : [...(prev[day] || []), index]
+      }));
+    }
   };
+
+
+  const subjects = [
+    { name: "Programming Logic" },
+    { name: "Linear algebra and analytical geometry"},
+    { name: "Agile Development" },
+    { name: "Data Structures"},
+  ];
 
   return (
     <div className="p-5">
-      <h2 className="text-2xl font-semibold mb-4">Inform Availability</h2>
-      <div className="flex justify-between">
-        <div className="overflow-hidden rounded-lg border border-gray-500">
-          <table className="table-auto border-collapse">
+      <TranslationButtons/>
+      <h2 className="text-center text-3xl text-black mb-8">Inform Availability</h2>
+      <div className="flex justify-between items-center mb-4 p-4 pb-0 pt-0">
+        <div className="flex items-center space-x-4">
+          <label className="flex items-center cursor-pointer">
+            <input type="radio" name="availability"   value="available" checked={selectedAvailability === 'available'} onChange={handleAvailabilityChange} className="w-4 h-4 mr-2" />
+            <span>Available</span>
+          </label>
+          <label className="flex items-center cursor-pointer">
+            <input type="radio" name="availability" value="unavailable" checked={selectedAvailability === 'unavailable'} onChange={handleAvailabilityChange} className="w-4 h-4  mr-2" />
+            <span>Unavailable</span>
+          </label>
+        </div>
+      </div>
+      <div className="flex">
+        <div className="w-full md:w-1/2 p-4 pt-0">
+          <table className="table-auto border-collapse border border-gray-500 rounded-lg w-full">
             <thead>
               <tr>
                 <th className="border border-gray-500 p-2"></th>
                 {days.map((day, index) => (
-                  <th style={{"width": 90}} key={index} className="border border-gray-500 p-2">{day}</th>
+                  <th key={index} className="border border-gray-500 p-2">{day}</th>
                 ))}
               </tr>
             </thead>
             <tbody>
               {times.map((time, timeIndex) => (
                 <tr key={timeIndex}>
-                  <td className="border border border-gray-500 p-2">{time}</td>
+                  <td className="border border-gray-500 p-2">{time}</td>
                   {days.map((day, dayIndex) => (
                     <td
                       key={dayIndex}
-                      className={`border border-gray-500 p-2 cursor-pointer ${availability[day.toLowerCase()]?.includes(timeIndex) ? 'bg-blue-500' : 'bg-white'} hover:bg-blue-200`}
-                      onClick={() => toggleAvailability(day.toLowerCase(), timeIndex)}
-                    >
-                    </td>
+                      className={`border border-gray-500 p-2 cursor-pointer ${availableCells[day]?.includes(timeIndex) ? 'bg-green-500' : availability[day]?.includes(timeIndex) ? 'bg-red-500' : 'bg-white'} hover:bg-blue-200`}
+                      onClick={() => toggleAvailability(day, timeIndex)}
+                    ></td>
                   ))}
                 </tr>
               ))}
@@ -59,8 +101,14 @@ const ScheduleTable: React.FC = () => {
           </table>
         </div>
       </div>
+      <div className="flex justify-between items-centermb-4 pl-4 pr-4">
+      <textarea placeholder="Observations" className="flex focus:outline-none text-xl w-[100%] h-24 p-2 rounded-lg bg-[#D9D9D9] align-top" rows={2} />
+      </div>
+      <div>
+        
+      </div>
     </div>
-  );
+    );
 };
 
 export default ScheduleTable;
