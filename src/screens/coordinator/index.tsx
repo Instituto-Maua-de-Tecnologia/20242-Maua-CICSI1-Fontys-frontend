bae50a754cbf2d18ecf2d89ffb3ca4133832cbfc
ZettDev <131803879+ZettDev@users.noreply.github.com>
2024-12-02 05:28:42 -0300

    schedule req init

--- src/screens/coordinator/index.tsx
+++ src/screens/coordinator/index.tsx
@@ -1,10 +1,11 @@
-import React, { useState, ChangeEvent } from 'react';
+import React, {useState, ChangeEvent, useContext} from 'react';
 import TranslationButtons from '@/components/translationButtons';
 import BtnCoordinator from '@/assets/images/btn-professor.png';
 import BtnCoordinator2 from '@/assets/images/btn-professor2.png';
 import LogoChat from '@/assets/images/chatLogo.png';
 import { useLanguage } from '@/components/languageProvider';
 import {useNavigate} from "react-router-dom";
+import {ScheduleContext} from "@/context/schedule_context.tsx";
 
 const translations = {
   en: {
@@ -40,16 +41,10 @@ function CoordinatorScreen() {
 
   const {currentLanguage} = useLanguage();
 
-  const [isPopupVisible, setPopupVisible] = useState(false);
-  const [selectedYearHalf, setSelectedYearHalf] = useState('');
-
-  const handleGenerateScheduleClick = () => {
-    setPopupVisible(true);
-  };
-
-  const handleClosePopup = () => {
-    setPopupVisible(false);
-  };
+  const [popupVisible, setPopupVisible] = useState(false);
+  const [algorithm, setAlgorithm] = useState(false);
+  const [selectedYearHalf, setSelectedYearHalf] = useState('1');
+  const { generateSchedule, generateWithAI } = useContext(ScheduleContext);
 
   const handleYearHalfChange = (event: ChangeEvent<HTMLInputElement>) => {
     setSelectedYearHalf(event.target.value);
@@ -62,6 +57,27 @@ function CoordinatorScreen() {
 
   const handleProfileClick = () => {
     navigate('/profile');
+  }
+
+  async function handleGenerateSchedule(){
+    try {
+      const response = await generateSchedule();
+      console.log(response);
+      return response;
+    }
+    catch (error: any) {
+      return error;
+    }
+  }
+
+  async function handleGenerateWithAI(){
+    try {
+      const response = await generateWithAI();
+      return response;
+    }
+    catch (error: any) {
+      return error;
+    }
   }
 
   return (
@@ -77,7 +93,10 @@ function CoordinatorScreen() {
           <button
               className="w-80 h-64 relative rounded-[30px] shadow-md transition-all duration-300 transform hover:scale-110 "
               style={{backgroundImage: `url(${BtnCoordinator})`, backgroundSize: 'cover', backgroundPosition: 'center'}}
-              onClick={handleGenerateScheduleClick}
+              onClick={() => {
+                setPopupVisible(true)
+                setAlgorithm(true)
+              }}
           >
             <div
                 className="absolute inset-0 rounded-[30px] flex justify-center items-center bg-black hover:bg-opacity-30 transition-all duration-300 opacity-0 hover:opacity-100">
@@ -121,64 +140,66 @@ function CoordinatorScreen() {
         </div>
         <button
             className="w-150 h-16 bg-[#000066] text-white rounded-[10px] shadow-md hover:shadow-lg flex items-center justify-center space-x-2 transition-all duration-300 transform hover:scale-110 px-6 py-2 "
-            onClick={handleGenerateScheduleClick}>
+            onClick={() => {
+              setPopupVisible(true)
+              setAlgorithm(false)
+            }}>
           <span className="text-xl">{translations[currentLanguage].buttonChatGPT}</span>
           <img src={LogoChat} alt="ChatGPT Logo" className="w-10"/>
 
         </button>
 
-        {isPopupVisible && (
-            <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50">
-              <div className="relative bg-white p-8 rounded-lg shadow-lg">
-                <button
-                    className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
-                    onClick={handleClosePopup}
-                >
-                  &times;
-                </button>
-                <h2 className="text-center text-2xl mb-4">{translations[currentLanguage].generateButton}</h2>
-                <div className="mb-4 flex items-center">
-                  <label className="block text-lg mb-0 mr-4">{translations[currentLanguage].course}</label>
-                  <select className="w-48 p-2 border rounded-lg">
-                    <option value="course1">{translations[currentLanguage].computerScience}</option>
-                    <option value="course2">{translations[currentLanguage].informationSystems}</option>
-                    <option value="course3">{translations[currentLanguage].AIDataScience}</option>
-                  </select>
-                </div>
-                <div className="mb-4 flex items-center">
-                  <label className="block text-lg mb-0 mr-4">{translations[currentLanguage].yearHalf}</label>
-                  <div className="flex space-x-4">
-                    <label className="flex items-center space-x-2">
-                      <input
-                          type="radio"
-                          name="yearHalf"
-                          value="1"
-                          checked={selectedYearHalf === '1'}
-                          onChange={handleYearHalfChange}
-                          className="form-radio bg-[#000066]"
-                      />
-                      <span>{translations[currentLanguage].first}</span>
-                    </label>
-                    <label className="flex items-center space-x-2">
-                      <input
-                          type="radio"
-                          name="yearHalf"
-                          value="2"
-                          checked={selectedYearHalf === '2'}
-                          onChange={handleYearHalfChange}
-                          className="form-radio bg-[#000066]"
-                      />
-                      <span>{translations[currentLanguage].second}</span>
-                    </label>
-                  </div>
-                </div>
-                <div className="flex justify-center">
-                  <button className="w-32 h-8 bg-[#000066] text-white rounded-[10px] shadow-md hover:shadow-lg">
-                    <div className="text-center text-x2">{translations[currentLanguage].generateButton}</div>
-                  </button>
+        {popupVisible && (
+          <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50">
+            <div className="relative bg-white p-8 rounded-lg shadow-lg">
+              <button
+                  className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
+                  onClick={() => setPopupVisible(false)}
+              >
+                &times;
+              </button>
+              <h2 className="text-center text-2xl mb-4">{translations[currentLanguage].generateButton}</h2>
+              <div className="mb-4 flex items-center">
+                <label className="block text-lg mb-0 mr-4">{translations[currentLanguage].course}</label>
+                <select className="w-48 p-2 border rounded-lg">
+                  <option value="course1">{translations[currentLanguage].computerScience}</option>
+                </select>
+              </div>
+              <div className="mb-4 flex items-center">
+                <label className="block text-lg mb-0 mr-4">{translations[currentLanguage].yearHalf}</label>
+                <div className="flex space-x-4">
+                  <label className="flex items-center space-x-2">
+                    <input
+                        type="radio"
+                        name="yearHalf"
+                        value="1"
+                        checked={selectedYearHalf === '1'}
+                        onChange={handleYearHalfChange}
+                        className="form-radio bg-[#000066]"
+                    />
+                    <span>{translations[currentLanguage].first}</span>
+                  </label>
+                  <label className="flex items-center space-x-2">
+                    <input
+                        type="radio"
+                        name="yearHalf"
+                        value="2"
+                        checked={selectedYearHalf === '2'}
+                        onChange={handleYearHalfChange}
+                        className="form-radio bg-[#000066]"
+                    />
+                    <span>{translations[currentLanguage].second}</span>
+                  </label>
                 </div>
               </div>
+              <div className="flex justify-center">
+                <button onClick={algorithm ? handleGenerateSchedule : handleGenerateWithAI}
+                    className="w-32 h-8 bg-[#000066] text-white rounded-[10px] shadow-md hover:shadow-lg">
+                  <div className="text-center text-x2">{translations[currentLanguage].generateButton}</div>
+                </button>
+              </div>
             </div>
+          </div>
         )}
       </div>
   );
