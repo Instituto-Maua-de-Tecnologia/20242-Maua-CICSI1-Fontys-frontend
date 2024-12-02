1afc2657ae9c6a89a25672c536c1450e273c1041
ZettDev <131803879+ZettDev@users.noreply.github.com>
2024-12-02 03:04:07 -0300

    upload excel and create user repository

--- src/api/repositories/user_repository_http.ts
+++ src/api/repositories/user_repository_http.ts
@@ -13,4 +13,26 @@ export class UserRepositoryHttp {
         }
     }
 
+    async uploadExcel(data: File){
+        const formData = new FormData();
+        formData.append('file', data);
+        try {
+            const response = await http.post('/upload_excel', formData);
+            console.log("uploadExcel response: ", response.data)
+            return response.data;
+        } catch (error: any) {
+            throw new Error(error)
+        }
+    }
+
+    async createUser(name: string){
+        try {
+            const response = await http.post('/users', { "name": name});
+            return response.data;
+        } catch (error: any) {
+            console.log("Create user respository error: ", error)
+            throw new Error(error)
+        }
+    }
+
 }
